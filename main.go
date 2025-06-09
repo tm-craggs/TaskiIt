8802db4162ac4b845f9e308f004167c441db75d3
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-06-09 17:33:26 +0100

    Add license info to main.go

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 /*
-Copyright © 2025 NAME HERE <tom.craggs@protonmail.comS>
+Copyright © 2025 Thomas Craggs <tom.craggs@protonmail.com>
+License: GPL 3.0 (see LICENSE file)
+
+TidyTask is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/gpl-3.0.html>
 */
+
 package main
 
 import (
