ce7a47220b511c90c7bdc751b84ba9963addf90f
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-06-18 22:10:14 +0100

    Add documentation to edit.go

--- cmd/edit.go
+++ cmd/edit.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 )
 
+// create struct that defines the available flags for edit command
 type editFlags struct {
 	title           string
 	due             string
@@ -17,6 +18,7 @@ type editFlags struct {
 	priorityChanged bool
 }
 
+// helper function to parse flags with error handling
 func getEditFlags(cmd *cobra.Command) (editFlags, error) {
 	var flags editFlags
 	var err error
@@ -43,55 +45,76 @@ func getEditFlags(cmd *cobra.Command) (editFlags, error) {
 	return flags, nil
 }
 
-// addCmd represents the add command
+// editCmd represents the edit command
 var editCmd = &cobra.Command{
-	Use:   "edit",
-	Short: "add all new task to your to-do list",
-	Long:  `Long description goes here`,
+	Use:   "edit [ID] [flags]",
+	Short: "Edit details of an existing task",
+	Long: `The 'edit' command allows you to update details of an existing task.
+
+Specify the task ID and pass flags for the the details you wish to change.`,
+
+	Example: `  tidytask edit 1 --title "Buy Groceries"
+	Change the title of task 1 to "Buy Groceries"
+
+  tidytask edit 3 --due 02-01-2006 --priority
+	Change the due date of task 3 to 2nd of January 2006, and toggle the priority status
+
+  tidytask edit 5 --title "Clean Room" --due 02-01-2006
+	Change the title of task 5 to Clean Room and change the due date to 2nd of January 2006`,
+
+	// backup database and confirm action before running command
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		if err := task.BackupDB(); err != nil {
-			return err
+			return fmt.Errorf("failed to backup database: %w", err)
 		}
 
 		if !util.ConfirmAction("Confirm Edit?") {
-			cmd.SilenceUsage = true
 			return fmt.Errorf("aborted by user")
 		}
 
 		return nil
 	},
+
+	// main command logic
 	RunE: func(cmd *cobra.Command, args []string) error {
 
+		// check if task ID has been provided
 		if len(args) == 0 {
-			return fmt.Errorf("no task ID provided")
+			return fmt.Errorf("task ID required")
 		}
 
+		// convert task ID to int
 		id, err := strconv.Atoi(args[0])
 		if err != nil {
 			return fmt.Errorf("invalid task ID %w", err)
 		}
 
+		// check task exists
 		if err := task.CheckTaskExists(id); err != nil {
 			return fmt.Errorf("task does not exist %w", err)
 		}
 
+		// get flags
 		flags, err := getEditFlags(cmd)
 		if err != nil {
 			return err
 		}
 
+		// update task title if title flagged
 		if flags.titleChanged {
 			if err := task.SetTitle(id, flags.title); err != nil {
 				return fmt.Errorf("failed to update title: %w", err)
 			}
 		}
 
+		// toggle task priority if priority flagged
 		if flags.priorityChanged {
 			if err := task.TogglePriority(id); err != nil {
 				return fmt.Errorf("failed to toggle priority: %w", err)
 			}
 		}
 
+		// update due date if due flagged
 		if flags.dueChanged {
 			if err := util.VerifyDate(flags.due); err != nil {
 				return fmt.Errorf("invalid due date: %w", err)
@@ -101,16 +124,20 @@ var editCmd = &cobra.Command{
 			}
 		}
 
+		// exit
 		fmt.Println("Task updated")
-
 		return nil
 	},
 }
 
+// command initialisation
 func init() {
-	editCmd.Flags().StringP("due", "d", "", "Set all due date (e.g. 2025-05-14)")
-	editCmd.Flags().BoolP("priority", "p", false, "Mark the task as high priority")
-	editCmd.Flags().StringP("title", "t", "", "Set all task title")
+
+	// define flags and add subcommand to root
+
+	editCmd.Flags().StringP("due", "d", "", "Change due date of task")
+	editCmd.Flags().BoolP("priority", "p", false, "Toggle the task priority")
+	editCmd.Flags().StringP("title", "t", "", "Change the title of task")
 
 	rootCmd.AddCommand(editCmd)
 }
