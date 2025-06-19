6921d1886a2af6f488b912f0d3ab8e00712f0eb6
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-06-19 02:02:51 +0100

    Add documentation to search.go

--- cmd/search.go
+++ cmd/search.go
@@ -7,6 +7,7 @@ import (
 	"github.com/tcraggs/TidyTask/util"
 )
 
+// create struct that defines the available flags for search command
 type searchFlags struct {
 	searchID       bool
 	searchTitle    bool
@@ -17,6 +18,7 @@ type searchFlags struct {
 	filterNormal   bool
 }
 
+// helper function to parse flags with error handling
 func getSearchFlags(cmd *cobra.Command) (*searchFlags, error) {
 	flags := &searchFlags{}
 	var err error
@@ -46,46 +48,81 @@ func getSearchFlags(cmd *cobra.Command) (*searchFlags, error) {
 	return flags, nil
 }
 
+// searchCmd represents the search command
 var searchCmd = &cobra.Command{
-	Use:   "search",
-	Short: "Add all new task to your to-do list",
-	Long:  `Long description goes here`,
+	Use:   "search [keyword] [flags]",
+	Short: "Search for tasks using a keyword",
+	Long: `The 'search' command is used to search for tasks using a keyword.
+
+By default, the keyword is matched against all fields: ID, title, and due date. 
+You can narrow the scope by explicitly specifying which fields to search using the --id, --title, and --due flags
+
+"You can also narrow results using constraint flags, which show only tasks that meet the criteria you specify."`,
+
+	Example: `  tidytask search essay
+  > Search all fields for the word 'essay'
+
+  tidytask search homework --title --priority
+  > Search for tasks that contain 'homework' in the title, showing only priority results, 
+
+  tidytask search 2024 --due --open --priority
+  > Search due dates for the number '2024', show only tasks that are both open and high-priority`,
+
 	RunE: func(cmd *cobra.Command, args []string) error {
+
+		// check if keyword provided
 		if len(args) == 0 {
-			return fmt.Errorf("no search word specified")
+			return fmt.Errorf("keyword required")
 		}
 
+		// get flags
 		flags, err := getSearchFlags(cmd)
 		if err != nil {
 			return err
 		}
 
+		// if no fields selected, search all
+		if !flags.searchID && !flags.searchTitle && !flags.searchDue {
+			flags.searchID = true
+			flags.searchTitle = true
+			flags.searchDue = true
+		}
+
+		// get keyword
 		keyword := args[0]
 
+		// search specified fields for the keyword
 		tasks, err := task.SearchTasks(keyword, flags.searchID, flags.searchTitle, flags.searchDue)
 		if err != nil {
 			return fmt.Errorf("failed searching tasks: %w", err)
 		}
 
+		// filter search results using flags
 		filteredTasks := util.FilterTasks(tasks, flags.filterComplete, flags.filterPriority, flags.filterOpen, flags.filterNormal)
 
+		// print tasks in table format
 		util.PrintTasks(filteredTasks)
 
+		// exit
 		return nil
 	},
 }
 
+// command initialisation
 func init() {
-	rootCmd.AddCommand(searchCmd)
+
+	// define flags and add subcommand to root
 
 	// target flags
-	searchCmd.Flags().BoolP("id", "i", false, "Search task by ID")
-	searchCmd.Flags().BoolP("title", "t", false, "Search task by title")
-	searchCmd.Flags().BoolP("due", "d", false, "Search task by due")
+	searchCmd.Flags().BoolP("id", "i", false, "Match keyword against task ID")
+	searchCmd.Flags().BoolP("title", "t", false, "Match keyword against task title")
+	searchCmd.Flags().BoolP("due", "d", false, "Match keyword against due date")
 
 	// filter flags
-	searchCmd.Flags().BoolP("complete", "c", false, "Search only complete tasks")
-	searchCmd.Flags().BoolP("open", "o", false, "Search only open tasks")
-	searchCmd.Flags().BoolP("priority", "p", false, "Search task by priority")
-	searchCmd.Flags().BoolP("normal", "n", false, "Search normal priority tasks")
+	searchCmd.Flags().BoolP("complete", "c", false, "Show only complete tasks")
+	searchCmd.Flags().BoolP("open", "o", false, "Show only open (incomplete) tasks")
+	searchCmd.Flags().BoolP("priority", "p", false, "Show only high priority tasks")
+	searchCmd.Flags().BoolP("normal", "n", false, "Show normal priority tasks")
+
+	rootCmd.AddCommand(searchCmd)
 }
