a6dddc650d507987ee6f2cc2bea1d7cd6ef4e1b7
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-06-19 03:50:20 +0100

    Improce documentation for undo.go

--- cmd/undo.go
+++ cmd/undo.go
@@ -8,28 +8,43 @@ import (
 )
 
 var undoCmd = &cobra.Command{
-	Use:   "undo",
-	Short: "add all new task to your to-do list",
-	Long:  `Long description goes here`,
+	Use:                   "undo",
+	DisableFlagsInUseLine: true,
+	Short:                 "Undo the previous action",
+	Long: `The 'undo' command restores the most recent backup of your task list.
+
+A backup is automatically created before any command that modifies tasks, restoring the backup will reverse that change. 
+
+Only one backup is stored at a time - it can only restore the most recent change
+
+After running the undo command, it becomes unavailable until a new change is made.`,
+
+	// confirm action before running command
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		if !util.ConfirmAction("Confirm Undo?") {
-			cmd.SilenceUsage = true
 			return fmt.Errorf("aborted by user")
 		}
 		return nil
 	},
+
+	// command logic
 	RunE: func(cmd *cobra.Command, args []string) error {
 
+		// attempt to restore backup, throw error if no backup file found
 		err := task.RestoreBackup()
 		if err != nil {
 			return fmt.Errorf("no backup found: %w", err)
 		}
 
+		// exit
 		return nil
 
 	},
 }
 
+// command initialisation
 func init() {
+
+	// add subcommand to root
 	rootCmd.AddCommand(undoCmd)
 }
